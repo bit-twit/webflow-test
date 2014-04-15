ac5207a8f0081caed975c0eb221897c24cb797be
iulian.plesoianu <bit_twit@yahoo.com>
2014-04-15 23:51:38 +0300

    Added webflow. Still not functional.

---
+++ src/main/java/org/bittwit/webflow/controller/HomeController.java
@@ -0,0 +1,54 @@
+package org.bittwit.webflow.controller;
+ 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    private static Map<String, String> urls = new HashMap<String, String>();
+
+    static {
+        urls.put("/rms-campaign", "/rms/rms-campaign");
+        urls.put("/gws-campaign", "/gws/gws-campaign");
+    }
+
+    @RequestMapping(value="/?", method = RequestMethod.GET)
+    public String campaign(ModelMap model, HttpServletRequest request) {
+ 
+        String requestUrl = request.getRequestURL().toString();
+        requestUrl.replace(request.getPathInfo(), "");
+        model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
+ 
+        //Spring uses InternalResourceViewResolver and return back index.jsp
+        return "index";
+    }
+
+    @RequestMapping(value="/welcome", method = RequestMethod.GET)
+    public String welcome(ModelMap model) {
+ 
+        model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
+ 
+        //Spring uses InternalResourceViewResolver and return back index.jsp
+        return "index";
+ 
+    }
+ 
+    @RequestMapping(value="/welcome/{name}", method = RequestMethod.GET)
+    public String welcomeName(@PathVariable String name, ModelMap model) {
+ 
+        model.addAttribute("message", "Maven Web Project + Spring Webflow - " + name);
+        return "index";
+ 
+    }
+ 
+}
---
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World ${message}!</h2>
+<a href="/webflow-test/rms-campaign">rms campaign</a>
+<a href="/webflow-test/gws-campaign">gws campaign</a>
+</body>
+</html>
